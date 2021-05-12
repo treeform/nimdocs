2e2689e233b937b5c28491ad106b9bdbddf19ab9
treeform <starplant@gmail.com>
2021-05-12 08:40:00 -0700

    error handle better

--- src/nimdocs.nim
+++ src/nimdocs.nim
@@ -1,61 +1,98 @@
-import times, osproc, mimetypes, asynchttpserver, asyncdispatch, print, strutils, strformat, os
+import times, osproc, mimetypes, asynchttpserver, asyncdispatch, print, strutils, strformat, os, strtabs
 
-var checkAgain: float64
+var
+  gitPullTime: float64
+  gitPullRateLimit = 60.0 # seconds
+  gitCloneTime: float64
+  gitCloneRateLimit = 5.0 # seconds
 
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
 
+  # Handle static files.
   if req.url.path == "/":
     await req.respond(Http200, "TODO: index", newHttpHeaders())
     return
-
   if req.url.path == "/favicon.ico":
     await req.respond(Http200, "TODO: icon", newHttpHeaders())
     return
 
+  # Handle github urls.
   let
     parts = req.url.path.strip(chars={'/'}).split('/')
     author = parts[0]
     repo = parts[1]
     gitUrl = &"https://github.com/{author}/{repo}"
 
+  # Only allow registered authors.
   if author notin @["treeform", "guzba", "nim-lang"]:
     await req.respond(Http404, &"<h1>404: https://github.com/{author}/* not allowed</h1>", newHttpHeaders({
       "Content-Type": "text/html"
     }))
     return
 
+  var needsDocs = false
+  var output = ""
+
+  proc showLog() {.async.} =
+    print 500
+    echo output
+    await req.respond(Http500, output, newHttpHeaders())
+
   discard existsOrCreateDir("repos" / author)
 
-  proc genDocs() =
-    let nimbleDevelop = &"nimble develop -y"
-    print nimbleDevelop
-    print execCmdEx(nimbleDevelop, workingDir = "repos" / author / repo)
-
-    let nimbeDoc = &"nim doc --index:on --project --out:docs --hints:off --git.url:{gitUrl} --git.commit:master src/{repo}.nim "
-    print nimbeDoc
-    print execCmdEx(nimbeDoc, workingDir = "repos" / author / repo)
-
-  if checkAgain + 10.0 < epochTime():
-    checkAgain = epochTime()
-    if existsDir("repos" / author / repo):
+  if existsDir("repos" / author / repo):
+    if gitPullTime + gitPullRateLimit < epochTime():
+      gitPullTime = epochTime()
       let gitUpdate = &"git pull"
       print gitUpdate
-      let output = execCmdEx(gitUpdate, workingDir = "repos" / author / repo)
-      print output
-      if output[0] in ["Already up-to-date.\n", "Already up to date.\n"]:
-        genDocs()
-
+      let res = execCmdEx(gitUpdate, workingDir = "repos" / author / repo)
+      output.add res[0]
+      print res
+      if res[1] != 0:
+        await showLog()
+        return
+      if res[0] notin ["Already up-to-date.\n", "Already up to date.\n"]:
+        needsDocs = true
     else:
+      output.add "Rate limiting git pull"
+
+  else:
+    if gitCloneTime + gitCloneRateLimit < epochTime():
+      gitCloneTime = epochTime()
       let gitClone = &"git clone {gitUrl} repos/{author}/{repo}"
       print gitClone
-      print execCmdEx(gitClone)
-
-      genDocs()
+      let res = execCmdEx(gitClone, env = {"GIT_TERMINAL_PROMPT": "0"}.newStringTable)
+      output.add res[0]
+      if res[1] != 0:
+        await showLog()
+        return
+      needsDocs = true
+    else:
+      output.add "Rate limiting git clone"
 
   if not existsDir("repos" / author / repo):
-    await req.respond(Http200, "generating...", newHttpHeaders())
+    await showLog()
     return
+
+  if needsDocs:
+    block:
+      let nimbleDevelop = &"nimble develop -y"
+      print nimbleDevelop
+      let res = execCmdEx(nimbleDevelop, workingDir = "repos" / author / repo)
+      if res[1] != 0:
+        output.add res[0]
+        await showLog()
+        return
+
+    block:
+      let nimbeDoc = &"nim doc --index:on --project --out:docs --hints:off --git.url:{gitUrl} --git.commit:master src/{repo}.nim "
+      print nimbeDoc
+      let res = execCmdEx(nimbeDoc, workingDir = "repos" / author / repo)
+      if res[1] != 0:
+        output.add res[0]
+        await showLog()
+        return
 
   var filePath = ""
   print parts.len, parts
