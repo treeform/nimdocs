36b287e74a79968fcd3e38d3aa82dc9165bd5115
Saa <73304228+Hi02hi@users.noreply.github.com>
2021-05-14 17:28:15 -0700

    misspelling

--- src/nimdocs.nim
+++ src/nimdocs.nim
@@ -2,7 +2,7 @@
 ##
 ## I got tried of building nim docs for all of my many projects. I create this site that auto generates the docs for me based on the url. Going to https://nimdocs.com/treeform/pixie will just clone the https://github.com/treeform/pixie and generate the docs with index for all files.
 ##
-## If is alpha quality, so if you want to be included in the allowed authors list, open a pull request.
+## It is alpha quality, so if you want to be included in the allowed authors list, open a pull request.
 ## ## Features:
 ##
 ## * Check if the docs are up to date with git.
