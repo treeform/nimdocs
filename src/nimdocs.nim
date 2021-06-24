869eca319b518fdac70fe609a89a7ae07d77104c
treeform <starplant@gmail.com>
2021-06-24 09:45:28 -0700

    Better site formatting.

--- src/nimdocs.nim
+++ src/nimdocs.nim
@@ -5,7 +5,9 @@
 ## I got tried of building nim docs for all of my many projects. I create this site that auto generates the docs for me based on the url. Going to https://nimdocs.com/treeform/pixie will just clone the https://github.com/treeform/pixie and generate the docs with index for all files.
 ##
 ## It is alpha quality, so if you want to be included in the allowed authors list, open a pull request.
-## ## Features:
+##
+## Features:
+## ---------
 ##
 ## * Check if the docs are up to date with git.
 ## * Clone new repositories if one just goes to them.
